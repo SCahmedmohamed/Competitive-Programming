ddf22ee94c7bbb48762ba1731118093c1826f93f
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 11:41:32 +0200

    Create A. Calculating Function.cpp

---
+++ codeforces/A. Calculating Function.cpp
@@ -0,0 +1,11 @@
+    // By Ahmed Mohamed Ibrahim . Nov/15/2021 22:02
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    #define ll long long
+     
+    int main() {
+    	ll n;
+    	cin >> n;
+    	cout << n/2-n%2*n;
+    }
