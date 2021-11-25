09cd4443f68d219dea298ae7fc851dd706ef45c8
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 11:52:33 +0200

    Create A. Twins.cpp

---
+++ codeforces/A. Twins.cpp
@@ -0,0 +1,19 @@
+    // By Ahmed Mohamed Ibarhim . Nov/07/2021 22:27
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main(){
+        int t,n=0,h=0,i;
+        cin>>t;
+        int a[t];
+        for(i = 0; i < t; i++)
+        {
+            cin>>a[i];
+            h+=a[i];
+        }
+        sort(a,a+t);
+        for(i=0;2*n<=h;i++)
+            n+=a[t-i-1];
+     
+        cout<<i<<endl;
+     
+    }
