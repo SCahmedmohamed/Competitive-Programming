18ca3f8444b906413659b01160ee398fd0d9babb
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 15:24:53 +0200

    Create A. Translation.cpp

---
+++ codeforces/A. Translation.cpp
@@ -0,0 +1,14 @@
+    // By Ahmed Mohamed Ibrahim . Oct/22/2021 12:40
+    #include <bits/stdc++.h>
+     
+    using namespace std;
+     
+    int main(){
+     
+    	string s1, s2;
+    	cin >> s1;
+    	cin >> s2;
+     
+    	reverse( s2 . begin(), s2 . end() );
+    	s1==s2? cout << "YES" << endl : cout << "NO" << endl;
+    }
