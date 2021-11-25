b5f141b7d8f9f1d66b6960e9430399064a2f9f4c
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 12:27:16 +0200

    Create C. Berpizza.cpp

---
+++ codeforces/C. Berpizza.cpp
@@ -0,0 +1,35 @@
+    // By Ahmed Mohamed Ibrahim . Oct/27/2021 18:56 
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main(){
+      int q;
+      cin>>q;
+      priority_queue<pair<int,int>>pq;
+      int idx=0,id=0;
+      vector<bool>vis(500005,false);
+      while(q--){
+        int a;
+        cin>>a;
+        if(a==1){
+          int b;
+          cin>>b;
+          pq.push({b,-id});
+          id++;
+      }
+      else{
+        if(a==2){
+          while(vis[idx]!=false){
+          idx++;
+          }
+          vis[idx]=true;
+          cout<<idx+1<<" ";
+       }
+       else{
+          while(vis[-pq.top().second]!=false) pq.pop();
+          vis[-pq.top().second]=true;
+          int temp=-pq.top().second;
+          cout<<temp+1<<" ";
+        }
+      }
+      }
+    }g
