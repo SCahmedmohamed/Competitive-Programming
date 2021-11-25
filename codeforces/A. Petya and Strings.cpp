80c1377012b0384fa17e905f6498ed892cf643d3
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 12:04:54 +0200

    Create A. Petya and Strings.cpp

---
+++ codeforces/A. Petya and Strings.cpp
@@ -0,0 +1,9 @@
+    // By Ahmed Mohamed Ibrahim . Nov/06/2021 23:20
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main(){
+        char s[105],y[105];
+        cin >> s >> y;
+        cout << stricmp(s,y);
+        return 0;
+    }
