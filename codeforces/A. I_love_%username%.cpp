4f5cc67bcaa0823c5b870fd9545e6f5d9b6a9bfd
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 15:23:32 +0200

    Create A. I_love_%username%.cpp

---
+++ codeforces/A. I_love_%username%.cpp
@@ -0,0 +1,13 @@
+    // By Ahmed Mohamed Ibrahim . Oct/22/2021 14:26
+    #include<bits/stdc++.h>
+    using namespace std;
+    int n,x,mx=-1,mn=1e9,ans;
+    int main(){
+    	cin>>n;
+    	while(n--){
+    		cin>>x;
+    		if(x>mx)mx=x,ans++;
+    		if(x<mn)mn=x,ans++;
+    	}
+    	cout<<ans-2;
+    }
