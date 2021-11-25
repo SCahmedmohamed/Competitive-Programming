84573b79322b855b9a9c7a14a5cfe3902852e98e
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 15:30:17 +0200

    Create B. Borze.cpp

---
+++ codeforces/B. Borze.cpp
@@ -0,0 +1,32 @@
+// By Ahmed Mohamed Ibrahim . Oct/18/2021 21:24
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+ string s,s2;
+ cin>>s;
+int len=s.length();
+for(int i=0;i<len;i++)
+
+{
+	if(s[i]=='.'){
+		s2+='0';
+	}
+	if(s[i]=='-'&&s[i+1]=='.'){
+		s2+='1';
+		++i;
+		
+	}
+	if(s[i]=='-'&&s[i+1]=='-'){
+		s2+='2';
+		++i;
+	}
+	
+} 
+ cout<<s2;
+ 
+ 
+ 
+ 	return 0;
+}
