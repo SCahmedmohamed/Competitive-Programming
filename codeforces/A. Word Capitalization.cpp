b293555e9c44416f1a0701985d2d0e9698971f1e
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-25 15:37:37 +0200

    Create A. Word Capitalization.cpp

---
+++ codeforces/A. Word Capitalization.cpp
@@ -0,0 +1,13 @@
+    // By Ahmed Mohamed Ibrahim . Oct/14/2021 21:53
+    #include <iostream>
+    #include <string>
+    using namespace std;
+     
+    int main()
+    {
+        string s;
+        cin >> s;
+        s[0] = towupper(s[0]);
+        cout << s << endl;
+        return 0;
+    }
