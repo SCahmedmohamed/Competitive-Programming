e2eb2f99b36a26da9e410ef4e4a727a3d24fa5af
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-27 08:02:04 +0200

    Create B. Array Cancellation.cpp

---
+++ codeforces/B. Array Cancellation.cpp
@@ -0,0 +1,10 @@
+// By Ahmed Mohamed Ibrahim . Oct/13/2021 21:54
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int T,N,a,c,v;
+int main()
+{
+         for(cin>>T;cin>>N;cout<<a<<' ')for(a=c=0;N--;a=max(a,c-=v))cin>>v;
+         }
+         
