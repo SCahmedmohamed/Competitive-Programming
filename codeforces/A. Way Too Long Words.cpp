955a54eac2cb689e329daf16023a22b219de70c6
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-27 08:21:23 +0200

    Create A. Way Too Long Words.cpp

---
+++ codeforces/A. Way Too Long Words.cpp
@@ -0,0 +1,24 @@
+// By Ahmed Mohamed Ibrahim . Oct/07/2021 14:52
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n;
+    while (n--)
+    {
+        cin >> s;
+        if (s.length() > 10)
+        {
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
