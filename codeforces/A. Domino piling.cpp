04a26510c97a196a5a680d38dcc2c620b24413ea
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-27 08:33:10 +0200

    Create A. Domino piling.cpp

---
+++ codeforces/A. Domino piling.cpp
@@ -0,0 +1,13 @@
+// By Ahmed Mohamed Ibrahim . Sep/16/2021 21:53
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int m , n;
+    cin >> m >> n
+
+    cout << ( m * n )/2 << endl;
+
+    return 0;
+}
