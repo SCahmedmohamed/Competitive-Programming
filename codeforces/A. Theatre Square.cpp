07a539c452785c7853f9f21966f128234355b650
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-27 08:18:16 +0200

    Create A. Theatre Square.cpp

---
+++ codeforces/A. Theatre Square.cpp
@@ -0,0 +1,11 @@
+// By Ahmed Mohamed Ibrahim . Oct/08/2021 21:06
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main() {
+ll n,m,a;
+cin>>n>>m>>a;
+ll value=ceil((double)m/a)*ceil((double)n/a);
+cout<<value ;
+	return 0;
+}
