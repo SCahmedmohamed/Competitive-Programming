5c0204de9caf7c4ae104a3e6c3e08dba1f1542d2
SCahmedmohamed <94641804+SCahmedmohamed@users.noreply.github.com>
2021-11-27 08:16:52 +0200

    Create A. Elephant.cpp

---
+++ codeforces/A. Elephant.cpp
@@ -0,0 +1,15 @@
+// By Ahmed Mohamed Ibrahim . Oct/09/2021 01:29
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int x, answer =0;
+    cin >> x;
+    while(x > 0){
+        answer++;
+        x = x-5;
+    }
+            cout << answer<< endl;
+ 
+}
