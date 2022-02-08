fc68f54a20872a113b68ce267e06b8406c222752
AhmedMohamedScorpion <94641804+SCahmedmohamed@users.noreply.github.com>
2022-02-09 00:05:50 +0200

    Create 199A. Hexadecimal's theorem.cpp

---
+++ codeforces/199A. Hexadecimal's theorem.cpp
@@ -0,0 +1,17 @@
+// By Ahmed Mohamed Ibrahim . Feb/09/2022 01:04
+#include <bits/stdc++.h>
+using namespace std;
+#define ts int test_cases;cin >> test_cases;while(test_cases--)
+#define ld long double
+#define ll long long
+#define el endl
+#define ft float
+#define Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int z;
+int main(){
+    Scorpion
+    int x;
+    cin >> x;
+    cout << 0 << " " << 0 << " " << x << el;
+    return 0;
+}
