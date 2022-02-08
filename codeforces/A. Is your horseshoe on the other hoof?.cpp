98e4a7172952c8c8ecb650fc486eca85c71ced58
AhmedMohamedScorpion <94641804+SCahmedmohamed@users.noreply.github.com>
2022-02-08 14:57:35 +0200

    Create A. Is your horseshoe on the other hoof?.cpp

---
+++ codeforces/A. Is your horseshoe on the other hoof?.cpp
@@ -0,0 +1,18 @@
+// By Ahmed Mohamed Ibrahim . Jan/30/2022 13:57
+#include <bits/stdc++.h>
+using namespace std;
+#define ts int test_cases;cin >> test_cases;while(test_cases--)
+#define ld long double
+#define ll long long
+#define el endl
+#define ft float
+#define _3m_Scorpion Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int  z , x , c , v;
+int main(){
+    int a ,b ,c ,d;
+    cin >> a >> b >> c >> d;
+    set<int>s = {a , b, c , d};
+    cout << 4 - s.size() << el;
+
+      return 0;
+}
