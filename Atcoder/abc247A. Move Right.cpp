4126cc19f346abb163db236957a13f69f3e57046
AhmedMohamedScorpion <94641804+SCahmedmohamed@users.noreply.github.com>
2022-04-16 08:44:00 +0200

    Create abc247A. Move Right.cpp

---
+++ Atcoder/abc247A. Move Right.cpp
@@ -0,0 +1,20 @@
+// By Ahmed Mohamed Ibrahim . 2022-04-16 08:42:33	
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+#define ll long long
+#define el endl
+#define ff first
+#define ss second
+#define Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 2e6 + 5;
+string s;
+int main(){
+    Scorpion
+    cin >> s;
+    cout << 0;
+    for(int i = 0; i < s.size() - 1; i++){
+        cout << s[i];
+    }
+    return 0;
+}
