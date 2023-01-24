2368d4213d3fbe6170f66cbfad527c3a8edb6dfa
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-01-24 14:59:19 +0200

    Create A. Say Hello With C++.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/sheet #1/A. Say Hello With C++.cpp
@@ -0,0 +1,24 @@
+// By Ahmed Mohamed Ibrahim . Jan/24/2023 15:52	
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e5 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+
+void solve(string s){
+    cout << "Hello, " << s;
+    return;
+}
+
+int main (){                           Scorpion
+    string s;
+    cin >> s;
+    solve(s);
+
+    return 0;
+}
+
