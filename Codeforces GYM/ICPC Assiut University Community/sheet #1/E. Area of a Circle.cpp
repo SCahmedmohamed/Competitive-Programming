3a9b7af34552f4ef5a381aa42a6be68e82b423d1
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-01-24 16:04:04 +0200

    Create E. Area of a Circle.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/sheet #1/E. Area of a Circle.cpp
@@ -0,0 +1,22 @@
+// By Ahmed Mohamed Ibrahim .Jan/24/2023 17:03	
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e5 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi = 3.141592653;
+int main (){                                Scorpion
+    cout << fixed << setprecision(9);
+    double x;
+    cin >> x;
+    double r = x*x;
+    double area = r * Pi;
+    cout << area << endl;
+    return 0;
+}
+
