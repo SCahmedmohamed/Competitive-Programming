dbfdeb6b957ec96c61787bca24edc7d52b8cd912
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-01-24 15:08:54 +0200

    Create D. Difference.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/sheet #1/D. Difference.cpp
@@ -0,0 +1,18 @@
+// By Ahmed Mohamed Ibrahim .Jan/24/2023 16:08	
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e5 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+
+int main (){                           Scorpion
+    ll a , b, c , d;
+    cin >> a >> b >> c >> d;
+    cout <<  "Difference = " << ((a * b) - (c * d)) << endl;
+    return 0;
+}
+
