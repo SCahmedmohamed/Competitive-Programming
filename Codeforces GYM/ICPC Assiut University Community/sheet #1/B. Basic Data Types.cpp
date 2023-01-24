c022459550e25ef573bff84224e6026466cd5964
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-01-24 15:01:26 +0200

    Create B. Basic Data Types.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/sheet #1/B. Basic Data Types.cpp
@@ -0,0 +1,26 @@
+// By Ahmed Mohamed Ibrahim . Jan/24/2023 16:01	
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e5 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+
+void solve(string s){
+    return;
+}
+
+int main (){                           Scorpion
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b>> c>> d >> e;
+    cout << a << el << b << el << c << el << d << el << e;
+    return 0;
+}
+
