506ca74a085e2f768a59be9f0f872832baabc4ee
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-01-26 15:28:07 +0200

    Create P. First digit !.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/sheet #1/P. First digit !.cpp
@@ -0,0 +1,23 @@
+// By Ahmed Mohamed Ibrahim .Jan/26/2023 16:27	
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define Scorpion ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e5 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi = 3.141592653;
+int main (){                                Scorpion
+    int x;
+    cin >> x;
+    while(x >= 10){
+        x /= 10;
+    }
+    if(x %2 == 0) cout << "EVEN" <<endl;
+    else cout << "ODD" << endl;
+    return 0;
+}
+
