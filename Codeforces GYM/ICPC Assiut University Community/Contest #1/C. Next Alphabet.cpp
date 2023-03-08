b6efcde52dded8d4661f55abdc4ad89b1cf7c572
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-03-08 22:56:49 +0200

    Create C. Next Alphabet.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/Contest #1/C. Next Alphabet.cpp
@@ -0,0 +1,23 @@
+// By Ahmed Mohamed Ibrahim . Feb/24/2023 17:42	
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define One_Piece_3mk ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e4 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi = 3.141592653;
+
+int main (){                                One_Piece_3mk
+    char ch;
+    cin >> ch;
+    if(ch == 'z') cout << 'a' << endl;
+    else{
+        ch++;
+        cout << ch;
+    }
+    return 0;
+}
+ 
