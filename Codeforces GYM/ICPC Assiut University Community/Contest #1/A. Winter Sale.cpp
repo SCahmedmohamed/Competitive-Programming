86ad8ea98d7d5db9ee29dc3d9f45377a15e3a4ce
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-03-08 22:55:32 +0200

    Create A. Winter Sale.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/Contest #1/A. Winter Sale.cpp
@@ -0,0 +1,21 @@
+// By Ahmed Mohamed Ibrahim . Feb/21/2023 22:15	
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define One_Piece_3mk ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e4 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi = 3.141592653;
+
+int main (){                                One_Piece_3mk
+    cout << setprecision(2) << fixed;
+    double d , p;
+    cin >> d >> p;
+    double dis = 1 -(d/100);
+    cout << p / dis << endl;
+    return 0;
+}
+ 
