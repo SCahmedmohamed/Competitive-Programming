73d5f66143beb94c26e8eb0ca3b1efe6469fdea3
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-03-09 20:12:03 +0200

    Create F. Multiplication table.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/Sheet #2/F. Multiplication table.cpp
@@ -0,0 +1,22 @@
+// By Ahmed Mohamed Ibrahim . Mar/09/2023 21:11
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define TC int t;cin>>t;while(t--)
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define One_Piece_3mk ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e4 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi =  3.141592653;
+
+int n , a[N] , mx;
+
+int main (){                                One_Piece_3mk
+    cin >> n;
+    for(int i = 1; i <= 12; i++){
+        cout << n << " * " << i << " = " << n*i << endl;
+    }
+R:  return 0;
+}
