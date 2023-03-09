b376f4d39688d22d87e8095c8ea8d08885842b19
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-03-09 18:49:54 +0200

    Create A. 1 to N.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/Sheet #2/A. 1 to N.cpp
@@ -0,0 +1,22 @@
+// By Ahmed Mohamed Ibrahim . Mar/09/2023 19:49	
+#include <iostream>
+//#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define TC int t;cin>>t;while(t--)
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define One_Piece_3mk ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e4 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi =  3.141592653;
+
+double x;
+
+int main (){                                One_Piece_3mk
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) cout << i << endl;
+R:  return 0;
+}
