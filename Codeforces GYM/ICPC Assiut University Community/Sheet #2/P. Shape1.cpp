47f3edefb976a1eff3620bb605ea49f50c1196d6
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-03-10 15:40:10 +0200

    Create P. Shape1.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/Sheet #2/P. Shape1.cpp
@@ -0,0 +1,26 @@
+// By Ahmed Mohamed Ibrahim .Mar/10/2023 16:37	
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define TC int t;cin>>t;while(t--)
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define One_Piece_3mk ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e4 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi =  3.141592653;
+
+ll t, n , a[N] , mx , mn;
+
+int main (){                                One_Piece_3mk
+    cin >> n;
+    for(int i = n; i >= 1; i--){
+        int p = i;
+        while(p--){
+            cout << '*';
+        }
+        cout << endl;
+    }
+R:  return 0;
+}
