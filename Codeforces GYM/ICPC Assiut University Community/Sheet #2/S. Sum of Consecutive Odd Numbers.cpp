194c46ae69e9aa07bb08d685cbc8b173bcf44f15
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-03-10 21:57:32 +0200

    Create S. Sum of Consecutive Odd Numbers.cpp

---
+++ Codeforces GYM/ICPC Assiut University Community/Sheet #2/S. Sum of Consecutive Odd Numbers.cpp
@@ -0,0 +1,33 @@
+// By Ahmed Mohamed Ibrahim .Mar/10/2023 22:56	
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define TC int t;cin>>t;while(t--)
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define One_Piece_3mk ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 1e4 + 5 , inf = 1e9 + 7 , NEG = -1e9;
+const double Pi =  3.141592653;
+
+ll t, n , a[N] , mx , mn, ans , x , y, z;
+string s;
+
+void bigger(int x , int y){
+    if(x > y) mx = x , mn = y;
+    else mx = y , mn = x;
+}
+
+int main (){                                One_Piece_3mk
+    TC{
+        ans = 0;
+        cin >> x >> y;
+        bigger(x,y);
+        for(int i = mn + 1; i < mx; i++){
+            if(i % 2 != 0) ans += i;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
