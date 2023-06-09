4cb48b3cb7e1133ee6c9323b3c4a7661d7a77ff0
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-06-09 22:59:52 +0300

    Create C. Bun Lover.cpp

---
+++ codeforces/C. Bun Lover.cpp
@@ -0,0 +1,27 @@
+// By Ahmed Mohamed Ibrahim .Apr/25/2023 20:24	
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define TC int t;cin>>t;while(t--)
+#define endl '\n'
+#define el '\n'
+#define F first
+#define S second
+#define One_Piece_3mk ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int N = 2e5 + 5 , inf = 1e9 + 7 , NEG = -1e9 - 7;
+const double Pi =  3.141592653;
+
+ll  mn , mx , x , y , z, a , b , c , d, ans , cnt , arr[N] , arr2[N] , n , m;
+char ch;
+string s;
+bool flag , fg1;
+int vis[N];
+
+int main (){                                       One_Piece_3mk
+    TC{
+        cin >> n;
+        n +=1;
+        cout << n*n+1 << el;
+}
+    return 0;
+}
