a96b85d84e6588280af56332768a7e9d24fb6409
Ahmed Mohamed Ibrahim <94641804+SCahmedmohamed@users.noreply.github.com>
2023-06-13 00:23:47 +0300

    Create Binary Search.cpp

---
+++ Algorithms/Binary Search.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n = 9;
+    int array[] = {12,60,15,59,15,53,31,19,24};
+    sort(array,array + n); // should be sorted
+    // after sorting array[] = {12,15,15,19,24,31,53,59,60};
+    int x = 59; // the target
+
+    int left = 0 , right = n - 1;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(array[mid] == x){
+            cout << "YES";
+            return;
+        }else{
+            if(array[mid] > x)  right = mid - 1;
+            //from mid to right are bigger than x
+            else
+                left = mid + 1;
+            // from left to mid are smaller than x
+        }
+    }
+    cout << -1; // when right > left and we didnt get the number
+}
+
+int main (){
+    solve();
+
+    return 0;
+}
